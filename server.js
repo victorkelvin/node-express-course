2f769a3686a818c68e2972a8a23446ae1126deed
unknown <victorkelvin@gmail.com>
2020-07-17 00:44:17 -0300

    add dynamic Get route

--- server.js
+++ server.js
@@ -5,15 +5,26 @@ myApp.listen(8040, function() {
     console.log("Hello World!")
 })
 
-const mockUserData=[
-	{name:'Victor'},
-	{name:'Kelvin'}
-]
-myApp.get('/users', function(req,res){
-	res.json({
-		success: true,
-		message: 'Sucessfully got users, Great!',
-		users: mockUserData
-	})
-})
+const mockUserData = [
+        { name: 'Victor' },
+        { name: 'Kelvin' }
+    ]
+    /*
+    myApp.get('/users', function(req,res){
+    	res.json({
+    		success: true,
+    		message: 'Sucessfully got users, Great!',
+    		users: mockUserData
+    	})
+    })
 
+    */
+
+myApp.get('/users/:id', function(req, res) {
+    console.log(req.params.id)
+    res.json({
+        sucess: true,
+        message: 'Got one user',
+        user: req.params.id
+    })
+})
