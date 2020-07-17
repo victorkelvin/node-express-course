93a0d2ed614d5c777a7e060edca38c963bef3265
victorkelvin <victorkelvin@DESKTOP-4198M05.localdomain>
2020-07-17 00:29:10 -0300

    add first GET route

--- server.js
+++ server.js
@@ -4,3 +4,16 @@ const myApp = express();
 myApp.listen(8040, function() {
     console.log("Hello World!")
 })
+
+const mockUserData=[
+	{name:'Victor'},
+	{name:'Kelvin'}
+]
+myApp.get('/users', function(req,res){
+	res.json({
+		success: true,
+		message: 'Sucessfully got users, Great!',
+		users: mockUserData
+	})
+})
+
